2a4984d83d20c0ee87381d2eb92cfa71d00b6131
sainttheforgotten <sainttheforgotten@proton.me>
2023-08-11 16:27:44 +0200

    Creating a GLFW window

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,56 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+const unsigned int WINDOW_WIDTH  = 800;
+const unsigned int WINDOW_HEIGHT = 600;
+const char* WINDOW_TITLE         = "Kedar Engine";
+
 int main()
 {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* window = glfwCreateWindow(
+    WINDOW_WIDTH,
+    WINDOW_HEIGHT,
+    WINDOW_TITLE,
+    NULL,
+    NULL
+  );
+  if (window == NULL)
+  {
+    std::cerr << "Failed to create a window!" << std::endl;
+    glfwTerminate();
+    return 1;
+  }
+  glfwMakeContextCurrent(window);
+
+  GLenum err = glewInit();
+  if (GLEW_OK != err)
+  {
+    std::cerr << "Failed to initialize GLEW!" << std::endl;
+    std::cerr << "Error: " << glewGetErrorString(err) << std::endl;
+    glfwTerminate();
+    return 1;
+  }
+
+  GLclampf red   = 0.0f;
+  GLclampf green = 0.3f;
+  GLclampf blue  = 0.3f;
+  GLclampf alpha = 1.0f;
+  glClearColor(red, green, blue, alpha);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
   return 0;
 }
